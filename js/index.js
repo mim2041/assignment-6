ac15d6f24a563d266f2d4185f8196225b7be7384
mim2041 <you@example.com>
2022-11-15 23:04:23 +0600

    updated

--- js/index.js
+++ js/index.js
@@ -40,7 +40,7 @@ const displaySingleData = (allNews) => {
   allNews.sort(function(a,b){
     return  b.total_view - a.total_view;
   });
-
+  newsContainer.innerHTML = '';
   allNews.forEach((news) => {
     const li = document.createElement("li");
     li.classList.add("w-full");
@@ -74,7 +74,7 @@ const displaySingleData = (allNews) => {
         <i class="fa-solid fa-star"></i>
       </div>
       <div>
-        <i id="show-details" class="fa-solid fa-arrow-right"></i>
+        <button id="show-details" onclick="showModalDetails(${news.category_id})" class=" type="button" data-modal-toggle="defaultModal" "><i class="fa-solid fa-arrow-right"></i></button>
       </div>
       </div>
       </div>
@@ -82,9 +82,15 @@ const displaySingleData = (allNews) => {
         `;
     newsContainer.appendChild(li);
     const newsLength = document.getElementById('print-length');
-    const length = allNews.length;
-    // console.log(length);
-    newsLength.innerText = `${length} items found for category Entertainment`
+    // const length = allNews.length;
+     newsLength.innerText = `${allNews.length} items found for this category.`;
+    console.log(length);
+    // if(length > 0){
+    //   newsLength.innerText = `${length} items found for category Entertainment`
+    // }
+    // else{
+    //   newsLength.innerText = "No items found!!"
+    // }
   });
   toggleSpinner(false);
   
@@ -100,58 +106,49 @@ const toggleSpinner = isLoading => {
   }
 }
 
-//  document.getElementById('show-details').addEventListener('click',function(){
-//     const modalSection = document.getElementById('modal-section');
-//     //modalSection.innerHTML = `
-//   //   <div id="defaultModal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full justify-center items-center">
-//   //     <div class="relative p-4 w-full max-w-2xl h-full md:h-auto">
-//   //         <!-- Modal content -->
-//   //         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-//   //             <!-- Modal header -->
-//   //             <div class="flex justify-between items-start p-4 rounded-t border-b dark:border-gray-600">
-//   //                 <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-//   //                     Terms of Service
-//   //                 </h3>
-//   //                 <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="defaultModal">
-//   //                     <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-//   //                     <span class="sr-only">Close modal</span>
-//   //                 </button>
-//   //              </div>
-//   //             <!-- Modal body -->
-//   //             <div class="p-6 space-y-6">
-//   //                 <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-//   //                     With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
-//   //                 </p>
-//   //                 <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-//   //                     The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
-//   //                 </p>
-//   //             </div>
-//   //             <!-- Modal footer -->
-//   //             <div class="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
-//   //                 <button data-modal-toggle="defaultModal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
-//   //                 <button data-modal-toggle="defaultModal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button>
-//   //             </div>
-//   //         </div>
-//   //     </div>
-//   // </div>
-//   //  `;
-//   });
-
-const loadDetails = (news_id) => {
-  const url = `https://openapi.programming-hero.com/api/news/${news_id}`
-  fetch(url)
-  .then(res => res.json())
-  .then(data => console.log(data.array))
+const showModalDetails = (all_id) => {
+  console.log(all_id);
+  const modalContainer = document.getElementById('defaultModal');
+  all_id.forEach(id => {
+    const div = document.createElement('div');
+    div.classList.add('relative');
+    modalContainer.innerHTML = `
+    <div class="relative p-4 w-full max-w-2xl h-full md:h-auto">
+          <!-- Modal content -->
+          <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+              <!-- Modal header -->
+              <div class="flex justify-between items-start p-4 rounded-t border-b dark:border-gray-600">
+                  <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                      Terms of Service
+                  </h3>
+                  <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="defaultModal">
+                      <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                      <span class="sr-only">Close modal</span>
+                  </button>
+              </div>
+              <!-- Modal body -->
+              <div class="p-6 space-y-6">
+                  <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                      With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
+                  </p>
+                  <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                      The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
+                  </p>
+              </div>
+              <!-- Modal footer -->
+              <div class="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
+                  <button data-modal-toggle="defaultModal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
+                  <button data-modal-toggle="defaultModal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button>
+              </div>
+          </div>
+      </div>
+    `
+    
+  })
 }
 
-const showModalDetails = (arrays) => {
-  console.log(arrays);
-  // arrays.forEach(array => {
-  //   // console.log(array);
-  // })
-}
 
-loadDetails('08');
+// loadDetails('08');
 
 loadAllCategory();
 loadSingleCategory('8');
